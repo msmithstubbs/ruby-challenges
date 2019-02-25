a6f6c02be12a726eeaea3c5d06062ad50a349aeb
Matt Stubbs <22266+msmithstubbs@users.noreply.github.com>
2019-02-26 09:51:09 +1000

    Change test name to SumNumbersTest
    The test case was using `LargestNumbersTest` which was causing some confusion.

--- tests/04_sum_numbers_test.rb
+++ tests/04_sum_numbers_test.rb
@@ -1,7 +1,7 @@
 require 'test/unit'
 require_relative '../04_sum_numbers'
 
-class LargestNumberTest < Test::Unit::TestCase
+class SumNumberTest < Test::Unit::TestCase
   def test_sum_numbes
     assert_equal(3, sum_numbers([1,1,1]))
     assert_equal(117, sum_numbers([5,2,100,0,10]))
