96b58d1121a297f7d1256024edef38bd4a63f4ab
Matt Stubbs <matt@yellowrobot.com.au>
2019-04-02 10:30:02 +1000

    Fix test method name

--- tests/26_max_min_test.rb
+++ tests/26_max_min_test.rb
@@ -2,7 +2,7 @@ require 'test/unit'
 require_relative '../26_max_min'
 
 class MaxMinInString < Test::Unit::TestCase
-  def test_no_duplicates
+  def test_max_min
     assert_equal  "23 1", max_min("5 2 9 8 23 1")
     assert_equal "38 -9", max_min("15 4 4 38 -9")
   end
